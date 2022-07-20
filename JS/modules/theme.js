73bfe9d77e28f0b7a3bedee16bbba93aa9e9cbbd
*Diogo <diogolimakas@hotmail.com>
2022-07-20 20:43:04 -0300

    add tema

---
+++ JS/modules/theme.js
@@ -0,0 +1,23 @@
+export default function theme() {
+
+
+
+  const check = document.querySelector('.liga-desliga_checkbox');
+
+  const themes = {
+    't-dark': 't-light',
+    't-light': 't-dark'
+  }
+
+
+  if (check) {
+    check.addEventListener('click', none)
+
+    function none() {
+      const currentTheme = document.body.dataset.theme;
+      document.body.setAttribute('data-theme', themes[currentTheme] || 't-light')
+    }
+  }
+
+
+}
--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
-import initActiveMenu from './modules/active-menu.js';
+import initTheme from './modules/theme.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 
 import initMenuMobile from './modules/menu-mobile.js';
 
+initTheme()
 initMenuMobile()
-initActiveMenu()
 initDropdownMenu()
