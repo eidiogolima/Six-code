f4b6cd5e647748303fd2aa7504de88cc2442267b
*Diogo <diogolimakas@hotmail.com>
2022-07-30 22:54:48 -0300

    palavras chaves e remoção das referências momentânea

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,9 @@
 import initTheme from './modules/theme.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
-
 import initMenuMobile from './modules/menu-mobile.js';
+import initActiveList from './modules/active-list.js';
 
 initTheme()
 initMenuMobile()
 initDropdownMenu()
+initActiveList()
